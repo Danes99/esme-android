125e19d26c64b7a1100bd91b129c5b87eb0ef4c3
thibault taxil <thibault.taxil@esme.fr>
2020-11-24 14:04:56 +0100

    commit de l'app thibault

---
+++ androidThibault/app/src/main/java/org/libreapps/thinkit/LikePost.java
@@ -0,0 +1,14 @@
+package org.libreapps.thinkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LikePost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like_post);
+    }
+}
